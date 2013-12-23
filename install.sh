05fcf0be80edc3f3c246f7c74ae2713f007ee25a
Jackson Owens <jackson_owens@brown.edu>
2013-12-23 13:22:20 -0500

    added packages, vim, golang

--- install.sh
+++ install.sh
@@ -1,1 +1,19 @@
-apt-get install gcc make
+#!/bin/bash
+
+# Install some necessities
+echo "Installing necessities"
+apt-get install -y gcc make git mercurial htop postgresql nginx python python3
+
+# Setup my vim preferences
+echo "Setting up vim"
+git clone https://github.com/jbowens/dotfiles.git
+cp ./dotfiles/.vimrc ~/.vimrc
+cp -r ./dotfiles/.vim ~/
+rm -rf dotfiles
+
+# Install go from source; Packages tend to be out of date
+# and go development is still going very fast.
+echo "Installing golang"
+hg clone -u release https://code.google.com/p/go
+cd go/src
+./all.bash
